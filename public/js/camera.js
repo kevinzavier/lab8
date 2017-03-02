65057590c97e0fe7f0ddfd61bd0c2f2e49f9ff7b
Kevin Zavier <kzavier@ucsd.edu>
2017-03-02 11:34:10 -0800

    thirdpartyAPI lab

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,8 +1,7 @@
 function initCamera() {
-  //Uncomment and fill in the correct selectors below.
-  //capture($('<Your code here>'),
-  //        $('<Your code here>'),
-  //        $('<Your code here>'));
+  capture($('#camera-video'),
+          $('#camera-canvas'),
+          $('#cameria-button'));
 }
 
 function capture(video, canvas, snapshotButton) {
@@ -38,4 +37,3 @@ function capture(video, canvas, snapshotButton) {
       successCallback, errorCallback);
 
 };
-
